ae8aaf8872299cae530fe75323f0e8aa56a64cfc
Yu Yinda <85948597@qq.com>
2019-07-04 15:44:33 +0800

    First Commit

---
+++ Assets/Resources/FpsView.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FpsView : MonoBehaviour
+{
+    public float updateInterval = 0.5F;
+    private double lastInterval;
+    private int frames = 0;
+    private float fps;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastInterval = Time.realtimeSinceStartup;
+        frames = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        ++frames;
+        float timeNow = Time.realtimeSinceStartup;
+        if (timeNow > lastInterval + updateInterval)
+        {
+            fps = (float)(frames / (timeNow - lastInterval));
+            frames = 0;
+            lastInterval = timeNow;
+        }
+        Text text = gameObject.GetComponent<Text>();
+        text.text = fps.ToString("f2");
+    }
+}
---
+++ Assets/Resources/FpsViewMain.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FpsViewMain : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var canvasObj = Resources.Load("Canvas", typeof(Canvas));
+        Canvas cube = Instantiate(canvasObj) as Canvas;
+        cube.name = "MainCanvas";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
